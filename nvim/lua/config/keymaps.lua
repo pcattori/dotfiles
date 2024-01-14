b7e4b3adbc80f6fd187033aafd8ddb1f87076f80
Pedro Cattori <pcattori@gmail.com>
2024-01-14 17:27:54 -0500

    nvim: link to macros video

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,6 @@ vim.keymap.set({ "i", "v" }, ";;", "<Esc>")
 -- backspace through whole words with <option> + <backspace>
 vim.keymap.set("i", "<M-Backspace>", "<C-W>")
 
--- quick macro playback
+-- quick macro playback : https://www.youtube.com/watch?v=5x3dXo8aDCI
 vim.keymap.set("n", "Q", "@qj")
 vim.keymap.set("x", "Q", ":norm @q<CR>")
