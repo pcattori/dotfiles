5bed2e2ba4c4184e4728b098877e96c5f57c774c
Pedro Cattori <pcattori@gmail.com>
2025-01-06 19:10:22 -0500

    install fresh copy of lazyvim v14

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,3 @@
-vim.keymap.set("i", "<S-Space>", "_")
-vim.keymap.set({ "i", "v" }, ";;", "<Esc>")
-
--- backspace through whole words with <option> + <backspace>
-vim.keymap.set("i", "<M-Backspace>", "<C-W>")
-
--- quick macro playback : https://www.youtube.com/watch?v=5x3dXo8aDCI
-vim.keymap.set("n", "Q", "@qj")
-vim.keymap.set("x", "Q", ":norm @q<CR>")
-
--- fix number toggling from LazyVim so that it doesn't forcibly enable relativenumber
-vim.keymap.set("n", "<leader>ul", ":set number!<CR>", { noremap = true, silent = true })
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,11 +1,3 @@
-vim.opt.conceallevel = 0
-vim.opt.listchars = {
-  trail = "·",
-  tab = "  ",
-  nbsp = "␣",
-  extends = "»",
-  precedes = "«",
-}
-
-vim.opt.number = false
-vim.opt.relativenumber = false
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
